86dc4afe33a80a6a80c5e8f19f59139ae05839dc
AlanNaoto <naoto321@gmail.com>
2019-11-15 12:47:48 +0100

    [general] added missing bounding box and sync scripts from last commit

---
+++ bb_filter.py
@@ -0,0 +1,91 @@
+"""
+Alan Naoto Tabata
+naoto321@gmail.com
+Created: 04/11/2019
+Updated: 14/11/2019
+"""
+import cv2
+
+
+def apply_filters_to_3d_bb(bb_3d_data, depth_array, sensor_width, sensor_height):
+    # Bounding Box processing
+    bb_3d_vehicles = bb_3d_data[0]
+    bb_3d_walkers = bb_3d_data[1]
+    # print('total vehicles on world', len(bb_3d_vehicles))
+    # print('total walkers on world', len(bb_3d_walkers))
+
+    # Depth + bb coordinate check
+    valid_bb_vehicles = proccess_3D_bb_with_depth(bb_3d_vehicles, depth_array, sensor_width, sensor_height)
+    valid_bb_walkers = proccess_3D_bb_with_depth(bb_3d_walkers, depth_array, sensor_width, sensor_height)
+    valid_bb_vehicles = transform_bb_3d_to_2d(valid_bb_vehicles, sensor_width, sensor_height)
+    valid_bb_walkers = transform_bb_3d_to_2d(valid_bb_walkers, sensor_width, sensor_height)
+
+    # # Saving results as imgs is optional
+    # rgb_img = cv2.imread(rgb)
+    # cv2.imwrite('raw_img.jpeg', rgb_img)
+    #
+    # for bb in valid_bb_vehicles:
+    #     cv2.rectangle(rgb_img, (bb[0], bb[1]), (bb[2], bb[3]), (0, 255, 0), 1)
+    # for bb in valid_bb_walkers:
+    #     cv2.rectangle(rgb_img, (bb[0], bb[1]), (bb[2], bb[3]), (0, 0, 255), 1)
+    #
+    # cv2.imwrite('filtered_boxed_img.jpeg', rgb_img)
+    # print('bb_walkers on img:', len(valid_bb_walkers))
+    # print('bb_vehicles on img:', len(valid_bb_vehicles))
+    return valid_bb_vehicles, valid_bb_walkers
+
+
+def proccess_3D_bb_with_depth(bb_3d_actors, depth_array, sensor_width, sensor_height):
+    valid_bbs = []
+    for actor in bb_3d_actors:
+        actor_bbs = []
+        for bb_xyz_point in actor:
+            x = int(bb_xyz_point[0]) - 1  # -1 is accounting for the x=1 position of the camera on the vehicle
+            y = int(bb_xyz_point[1])
+            z = bb_xyz_point[2] - 2  # -2 is accounting for the Z=2 position of the camera on the vehicle
+
+            # These limits stretching are done so that the vehicles' bounding boxes appear whole even if only half of
+            # the car is shown
+            softening_thresh = 200
+            if -softening_thresh <= x < 0:
+                x = 0
+            elif sensor_width < x <= sensor_width + softening_thresh:
+                x = sensor_width
+
+            if -softening_thresh <= y < 0:
+                y = 0
+            elif sensor_height < y <= sensor_height + softening_thresh:
+                y = sensor_height
+
+            # Check if its inside the camera dimensions
+            if 0 <= x <= sensor_width and 0 <= y <= sensor_height:
+                # Allow for some stretching on the z axis as well (a vehicle could be driving against us and thus get
+                # only half of its body)
+                if -1 <= z < 0:
+                    z = 0
+                # Check if its behind something compared to the depth img (if its in front, i.e. nearer to us, then ok)
+                if 0 <= z <= depth_array[y-1][x-1]:  # y and x on depth array are inverted for some reason
+                    actor_bbs.append([x, y])
+        if not actor_bbs == []:
+            valid_bbs.append(actor_bbs)
+    return valid_bbs
+
+
+def transform_bb_3d_to_2d(bb_3d_actor, sensor_width, sensor_height):
+    valid_bbs = []
+    for actor in bb_3d_actor:
+        x_data = [x[0] for x in actor]
+        y_data = [x[1] for x in actor]
+        x_min = min(x_data)
+        y_min = min(y_data)
+        x_max = max(x_data)
+        y_max = max(y_data)
+        shape = (x_max-x_min, y_max-y_min)
+        size = shape[0]*shape[1]
+        proportion = size/(sensor_height*sensor_width)
+        # print('shape {0}; size {1}; proportion {2}'.format(shape, size, proportion))
+        # Avoiding to get very small bounding boxes
+        if proportion > 3E-4:  # 3E-4 was a value found by observation
+            valid_bbs.append([x_min, y_min, x_max, y_max])
+    return valid_bbs
+
---
+++ set_synchronous_mode.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2019 Computer Vision Center (CVC) at the Universitat Autonoma de
+# Barcelona (UAB).
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+import sys
+import os
+MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
+CARLA_DIR = os.path.join(os.path.dirname(MAIN_DIR), 'CARLA_0.9.6')
+CARLA_EGG_PATH = os.path.join(CARLA_DIR, 'PythonAPI', 'carla', 'dist', 'carla-0.9.6-py3.5-linux-x86_64.egg')
+sys.path.append(CARLA_EGG_PATH)
+import carla
+import random
+
+import numpy as np
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+
+class CarlaSyncMode(object):
+    """
+    Context manager to synchronize output from different sensors. Synchronous
+    mode is enabled as long as we are inside this context
+
+        with CarlaSyncMode(world, sensors) as sync_mode:
+            while True:
+                data = sync_mode.tick(timeout=1.0)
+
+    """
+
+    def __init__(self, world, *sensors, **kwargs):
+        self.world = world
+        self.sensors = sensors
+        self.frame = None
+        self.delta_seconds = 1.0 / kwargs.get('fps', 20)
+        self._queues = []
+        self._settings = None
+
+    def __enter__(self):
+        self._settings = self.world.get_settings()
+        self.frame = self.world.apply_settings(carla.WorldSettings(
+            no_rendering_mode=False,
+            synchronous_mode=True,
+            fixed_delta_seconds=self.delta_seconds))
+
+        def make_queue(register_event):
+            q = queue.Queue()
+            register_event(q.put)
+            self._queues.append(q)
+
+        make_queue(self.world.on_tick)
+        for sensor in self.sensors:
+            make_queue(sensor.listen)
+        return self
+
+    def tick(self, timeout):
+        self.frame = self.world.tick()
+        data = [self._retrieve_data(q, timeout) for q in self._queues]
+        assert all(x.frame == self.frame for x in data)
+        return data
+
+    def __exit__(self, *args, **kwargs):
+        self.world.apply_settings(self._settings)
+
+    def _retrieve_data(self, sensor_queue, timeout):
+        while True:
+            data = sensor_queue.get(timeout=timeout)
+            if data.frame == self.frame:
+                return data
